6145d31178078f7e0e69b244c4c87e7cec77a013
pnrjulius <patricknrjulius@gmail.com>
2015-10-16 23:13:29 -0400

    Codename Space Jam begins

---
+++ Codename Space Jam/Assets/Player1Controller.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class BasicShip : MonoBehaviour
+{
+    private Vector3 deltav;
+    private Rigidbody body;
+    public float thrust;
+    public float turnrate;
+    private Vector3 yaxis = new Vector3(0.0f, 1.0f, 0.0f);
+    private float horizontal;
+    private float vertical;
+
+    // Use this for initialization
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        deltav.x = horizontal;
+        deltav.z = vertical;
+        deltav.y = 0;
+
+        body.velocity = body.velocity + thrust * deltav;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(deltav, yaxis), turnrate * Time.deltaTime);
+    }
+
+    public void SetHorizontal(float in_horizontal)
+    {
+        horizontal = in_horizontal;
+    }
+
+    public void SetVertical(float in_vertical)
+    {
+        vertical = in_vertical;
+    }
+}
+
+public class Player1Controller : MonoBehaviour {
+    BasicShip OurShip;
+
+    void Start()
+    {
+      OurShip  = new BasicShip();
+    }
+
+    void FixedUpdate()
+    {
+       OurShip.SetHorizontal(Input.GetAxis("Horizontal"));
+       OurShip.SetVertical(Input.GetAxis("Vertical"));
+    }
+}
