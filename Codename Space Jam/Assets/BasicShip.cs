146a06ef63d2ef2131ff3f17c4363d3b7edb65ad
pnrjulius <patricknrjulius@gmail.com>
2015-10-16 23:46:30 -0400

    Moves, but won't accept new values from the Inspector

--- Codename Space Jam/Assets/BasicShip.cs
+++ Codename Space Jam/Assets/BasicShip.cs
@@ -4,29 +4,30 @@ using System.Collections;
 [System.Serializable]
 public class BasicShip : MonoBehaviour {
 
-    private Vector3 deltav;
-    private Rigidbody body;
-    public float thrust = 0.1f;
-    public float turnrate = 2;
+    public float thrust;
+    public float turnrate;
     private Vector3 yaxis = new Vector3(0.0f, 1.0f, 0.0f);
     private float horizontal;
     private float vertical;
+    private Vector3 deltav;
+    private Rigidbody body;
 
     // Use this for initialization
     void Start()
     {
+        thrust = 0.2f;
+        turnrate = 2f;
         body = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
-        print(horizontal);
         deltav.x = horizontal;
         deltav.z = vertical;
         deltav.y = 0;
 
         body.velocity = body.velocity + thrust * deltav;
-        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(deltav, yaxis), turnrate * Time.deltaTime);
+        body.rotation = Quaternion.Slerp(body.rotation, Quaternion.LookRotation(deltav, yaxis), turnrate * Time.deltaTime);
     }
 
     public void SetHorizontal(float in_horizontal)
