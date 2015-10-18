bc2676ea9891bcfcb0eb543b9710ee392518409b
TemporalDissonance <dnross@umich.edu>
2015-10-18 16:10:24 -0400

    Added asteroid prefab & script

---
+++ Codename Space Jam/Assets/Asteroid.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * Random.Range(0.05f,1.0f);
+		gameObject.GetComponent<Rigidbody>().velocity = Random.insideUnitSphere * Random.Range(0.05f,1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
