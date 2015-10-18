18d4a27d8d2a88db50e09635f4583328e7193623
pnrjulius <patricknrjulius@gmail.com>
2015-10-17 21:02:45 -0400

    It will now say who wins!
    At least, once we find a way to pass the value of "winningplayer" to
    this scene when the main scene transitions to it.

---
+++ Codename Space Jam/Assets/GameOverMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverMenu : MonoBehaviour {
+    public int winningplayer;
+    private UnityEngine.UI.Text titletext;
+    private GUI_Canvas ourcanvas;
+    private UnityEngine.UI.Image background;
+    private Title thetitle;
+
+	// Use this for initialization
+	void Start () {
+
+        thetitle = gameObject.GetComponentInChildren<Title>();
+
+        if(winningplayer == 1)
+        {
+            thetitle.ChangeTitle("Player 1 Wins!");
+        }
+        else if(winningplayer == 2)
+        {
+            thetitle.ChangeTitle("Player 2 Wins!");
+        }
+        else
+        {
+            thetitle.ChangeTitle("It's a tie!");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Submit"))
+        {
+            Application.LoadLevel("launch");
+        }
+    }
+}
---
+++ Codename Space Jam/Assets/Title.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+    private UnityEngine.UI.Text thetext;
+
+    public void ChangeTitle(string in_title)
+    {
+        thetext = gameObject.GetComponent<UnityEngine.UI.Text>();
+
+        thetext.text = in_title;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
