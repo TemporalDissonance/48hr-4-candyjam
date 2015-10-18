0c8c86b00c4e551661c94091d72b07b7a9356be7
pnrjulius <patricknrjulius@gmail.com>
2015-10-18 12:23:36 -0400

    New Game Over screen
    The new Game Over screen displays some explanation of the aftermath
    (eventually we might want to pull it from a text file).

---
+++ Codename Space Jam/Assets/Explanation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explanation : MonoBehaviour {
+    private UnityEngine.UI.Text thetext;
+
+    public void AddExplanation(string in_explanation)
+    {
+        thetext = gameObject.GetComponent<UnityEngine.UI.Text>();
+
+        thetext.text = in_explanation;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Codename Space Jam/Assets/GameOverMenu.cs
+++ Codename Space Jam/Assets/GameOverMenu.cs
@@ -6,23 +6,29 @@ public class GameOverMenu : MonoBehaviour {
     private GUI_Canvas ourcanvas;
     private UnityEngine.UI.Image background;
     private Title thetitle;
+    private Explanation theexplanation;
 
 	// Use this for initialization
 	void Start () {
 
         thetitle = gameObject.GetComponentInChildren<Title>();
+        theexplanation = gameObject.GetComponentInChildren<Explanation>();
 
         if(Player.winningplayer == 1)
         {
             thetitle.ChangeTitle("Player 1 Wins!");
+            theexplanation.AddExplanation("With the Earth ship hovering above, Mars had no choice but to surrender. At last, the war was over.");
         }
         else if(Player.winningplayer == 2)
         {
             thetitle.ChangeTitle("Player 2 Wins!");
+            theexplanation.AddExplanation("When the Mars ship arrived, Earth surrendered and gave up all claim to Mars. At last, Mars was free.");
+            
         }
         else
         {
             thetitle.ChangeTitle("It's a tie!");
+            theexplanation.AddExplanation("With both starships disabled but far too expensive to replace, both planets looked on in despair.");
         }
     }
 	
